d3090b2ca59e8ce5dbe1307dbdbb9cfebdf60419
mofink <mosheafink@gmail.com>
2019-05-23 11:28:17 -0400

    Create non_repeated_linear.cpp

---
+++ algorithms/bitwise/non_repeated_linear.cpp
@@ -0,0 +1,25 @@
+//find non-repeated element in array in linear time
+
+#include <iostream>
+#include <vector>
+
+
+int no_double(std::vector<int>& vec)
+{
+	//the trick is to recall that n^1 = ~n and n^0 = n
+
+	int num = 0;
+	for (int i: vec)
+	{
+		num = num^i;
+	}
+	return num;
+}
+
+int main()
+{
+	std::vector<int> vec = {1, 2, 5, 4, 6, 8, 9, 2, 1, 4, 5, 8, 9};
+	std::cout << no_double(vec) << std::endl;
+	return 0;
+}
+
